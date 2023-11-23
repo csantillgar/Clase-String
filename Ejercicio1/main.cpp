1d00a77922c9a2a8173b9ae2c3b9c6141b41b2c7
csantillgar <csantill@myuax.com>
2023-11-23 12:26:27 +0100

    ejercicio1

---
+++ Ejercicio1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Declarar e inicializar una cadena utilizando la clase string
+    string frase = "Hola Ruben, apruebame porfi";
+
+    // Imprimir la cadena en la consola
+    cout << "La frase es: " << frase << endl;
+
+    return 0;
+}
